296055ccfa4c4f7f85b7e18488489b49708aa7cf
codedwells <wells.mail.13@gmail.com>
2023-06-13 11:52:51 +0300

    📦 NEW: file creation app

--- fs/app.js
+++ fs/app.js
@@ -1,3 +1,58 @@
-import fs from 'fs';
+import fs from 'fs/promises';
 
-fs.copyFileSync('./Snap.png', './copied.png',);
+(async () => {
+    const watcher = fs.watch('./command.txt');
+
+    const COMMANDS = {
+        CREATE_FILE: 'create a file'
+    };
+
+    const createFile = async (path) => {
+        try {
+            // Check if we already have the file
+            const isExisting = fs.open(path, 'r');
+            isExisting.close();
+
+            if (isExisting) {
+                console.log(`The file path ${path} already exists!!`);
+            }
+        } catch (e) {
+            let createHandler = fs.writeFile(path, 'Our new file has been created');
+            
+        }
+    };
+
+    for await (const event of watcher) {
+        let commandFileHandler = await fs.open(event.filename, 'r');
+
+        commandFileHandler.on('change', async () => {
+            // Size of file to be read
+            const fileSize = (await commandFileHandler.stat()).size;
+
+            // Read file options
+            const buff = Buffer.alloc(fileSize);
+            const length = buff.byteLength;
+            const position = 0;
+            const offset = 0;
+
+            // Read the file
+            await commandFileHandler.read(buff, offset, length, position);
+
+            // close file handler
+            commandFileHandler.close();
+
+            // Decoder
+            const command = buff.toString('utf-8').toLowerCase().trim();
+
+            if (command.includes('create a file')) {
+                const filePath = command.split(' ');
+
+                createFile(filePath[filePath.length - 1]);
+            }
+        });
+
+        if (event.eventType === 'change' && event.filename === 'command.txt') {
+            commandFileHandler.emit('change');
+        }
+    }
+})();
